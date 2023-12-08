c35854f90961c9ae9edd47b8d3ad58b554e4ccc7
patrice ELIE DIT COSAQUE <patriceedc@hotmail.com>
2023-12-08 14:47:52 +0100

    TP 04 Algorithmie

--- bases_algorithmie.py
+++ bases_algorithmie.py
@@ -1,6 +1,32 @@
 # Exercice 1
-nombre_pairs = 0
+
+for i in range(10):
+   # if i % 2 == 0:
+        #print(i)
+
+    if i % 2 == 1:
+        print(i)
 
 
 # Exercice 2
+
+# Déclaration de la liste
+liste = [1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4]
+
+# Affichage de tous les nombres de la liste grâce à une boucle
+for nombre in liste:
+    print(nombre)
+
+# Affichage uniquement des nombres positifs grâce à une boucle combinée à un if
+for nombre in liste:
+    if nombre > 0:
+        print(nombre)
+
+# Trouvez un moyen de calculer le nombre d’éléments positifs et d’afficher ce nombre.
+nombre_positif = 0
+for nombre in liste:
+    if nombre > 0:
+        nombre_positif += 1
+        print(nombre_positif)
+
 
--- variable.py
+++ variable.py
@@ -3,16 +3,16 @@ decimal = 12.5
 string = "Michel"
 boolean = True
 print("Magaly a eu une note de " + str(number) + "/20 en Histoire et Geographie")
-print("lavaleur est de type", type(number))
+print("la valeur est de type", type(number))
 
 print("La bouteille de vin du Pic saint Loup coûte " + str(decimal) + " euros")
-print("lavaleur est de type", type(decimal))
+print("la valeur est de type", type(decimal))
 
 print("Comment vas-tu " + string + " ?")
-print("lavaleur est de type", type(string))
+print("la valeur est de type", type(string))
 
-print( "La valeur de ma variable boolean est :", boolean)
-print("lavaleur est de type", type(boolean))
+print("La valeur de ma variable boolean est :", boolean)
+print("la valeur est de type", type(boolean))
 
 resultat = f"Monsieur {string} a eu une note de {number} sur 20 et une moyenne générale de {decimal}"
 print(resultat)
