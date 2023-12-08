a7183235292f4814c2b03199f8712b7036262dc6
patrice ELIE DIT COSAQUE <patriceedc@hotmail.com>
2023-12-08 11:20:22 +0100

    TP 03 operateurs

---
+++ bases_operateurs.py
@@ -0,0 +1,13 @@
+#Testez les operrateurs AND ET OR
+a = 10
+b = 20
+testEt = a > 0 and b < 10
+testOu = a > 0 or b < 10
+print(testEt)
+print(testOu)
+
+#Testez les operateurs combines
+a = a + 1
+print(a)
+
+
