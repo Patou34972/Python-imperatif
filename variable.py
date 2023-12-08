e32addc6050201b62905e57e9961457bf9eff238
patrice ELIE DIT COSAQUE <patriceedc@hotmail.com>
2023-12-08 11:01:50 +0100

    TP 02 variable

---
+++ variable.py
@@ -0,0 +1,8 @@
+number = 12
+decimal = 12.5
+string = "Michel"
+boolean = True
+print("Magaly a eu une note de " + str(number) + "/20 en Histoire et Geographie")
+print("La bouteille de vin du Pic saint Loup coûte " + str(decimal) + " euros")
+print("Comment vas-tu " + string + " ?")
+print( "La valeur de ma variable boolean est :", boolean)
